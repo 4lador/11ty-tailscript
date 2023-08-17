0beda4cce622204dcbac93ef3b4b4b00fe1fc567
4lador <4lador@gmail.com>
2023-08-17 16:38:06 +0200

    Code cleanup

--- tools/scripts/project-prefix-rename.js
+++ tools/scripts/project-prefix-rename.js
@@ -5,11 +5,11 @@ const dirConfig = require('../../eleventy.dirs.config').dir;
 
 const args = process.argv.slice(2);
 
-const oldPrefix = args.length <= 1 ? 'etail' : args[0];
+const oldPrefix = args.length <= 1 ? 'tailscript' : args[0];
 const newPrefix = args.length === 1 ? args[0] : args[1];
 
 const dirPaths = [
-    './src/_includes/components',
+    './src/_includes/components'
 ];
 
 const dirsPrefixRx = new RegExp(`(${oldPrefix})-(.+)`);
@@ -17,15 +17,8 @@ const filesPrefixRx = new RegExp(`(${oldPrefix})-(.+)\.(.+)`);
 const dirHasPrefixFilter = name => name.match(dirsPrefixRx);
 const fileHasPrefixFilter = name => name.match(filesPrefixRx)
 
-const getDirectories = async source => await getElement(source, (dirent) => dirent.isDirectory());
 const getPrefixedDirectories = async source => await getElement(source, (dirent) => dirent.isDirectory() && dirHasPrefixFilter(dirent.name));
 const getPrefixedFiles = async source => await getElement(source, (dirent) => dirent.isFile() && fileHasPrefixFilter(dirent.name));
-const getWebcFiles = async source => await getElement(source, dirent => {
-    if (!dirent.isFile()) return false;
-
-    const file = parse(join(dirent.path, dirent.name));
-    return file.ext && file.ext === '.webc';
-});
 
 const getElement = async (source, filter) => (await readdir(source, {withFileTypes: true}))
     .filter(filter)
@@ -57,22 +50,16 @@ function flatten(lists) {
         return newPath;
     }));
 
-    // Rewrite components tags in HTML
-    const codeDirectory = join(dirConfig.input, dirConfig.includes);
-    const codeDirectories = [dirConfig.input, ...(await getDirectories(codeDirectory)).map(dir => join(codeDirectory, dir))];
-
-    const templates = flatten(await Promise.all(codeDirectories.map(async dir => await getWebcFiles(dir))));
-
     const results = await replace({
         files: `${dirConfig.input}/**/*.webc`,
         from: new RegExp(`${oldPrefix}`, 'g'),
         to: newPrefix
     });
 
-    console.log('results', results);
+    console.log('WebC template tags renamed', results);
+    console.log(renamed.length + ' renamed elements', renamed);
 
-    console.log('templates', templates);
-    console.log('renamedDirs', renamed);
+    console.log('Directory / Files / WebC Tags: project prefixes renamed successfully !');
 })().catch(err => {
     console.error(err);
 });
