3251c5061fcb8fedbeb0892074685729faf2c30b
4lador <4lador@gmail.com>
2023-10-12 00:11:05 +0200

    WIP Collection list

---
+++ src/_includes/components/tailscript-blog-list-item/tailscript-blog-list-item.js
@@ -0,0 +1,22 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(self, () => {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*************************************************************************************!*\
+  !*** ./_includes/components/tailscript-blog-list-item/tailscript-blog-list-item.ts ***!
+  \*************************************************************************************/
+
+
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
